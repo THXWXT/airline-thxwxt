bfa94e600c0c910cfa4ee81176de71a2270b784e
6204062630513 <s6204062630513@email.kmutnb.ac.th>
2022-03-06 18:41:25 +0700

    'thx'

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
-const express = require('express');
-const path = require('path');
-
+const path = require("path");
+const express = require("express");
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/flight'));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/flight/'}),
-);
-
+app.use(express.static(__dirname + '/flight'));
+app.get('/*', function(req,res){
+res.sendFile(path.join(__dirname, 'flight', 'index.html'))
+});
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
